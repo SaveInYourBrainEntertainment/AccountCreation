70844221562d43aa6efb1752a329eb95ce4edddc
Soe Myat Min <soemyatmin.nathan@gmail.com>
2016-06-16 06:52:08 +0630

    BasicProjectSetting
    CloudBase

---
+++ Assets/Account.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Account: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int totalPotions = 5;
+		int totalCoins = 100;
+		string weaponID = "Weapon_102";
+		Analytics.CustomEvent("gameOver", new Dictionary<string, object>
+			{
+				{ "potions", totalPotions },
+				{ "coins", totalCoins },
+				{ "activeWeapon", weaponID }
+			});
+		Analytics.Transaction("12345abcde", 0.99m, "USD", null, null);
+
+		Gender gender = Gender.Female;
+		Analytics.SetUserGender(gender);
+
+		int birthYear = 2014;
+		Analytics.SetUserBirthYear(birthYear);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
